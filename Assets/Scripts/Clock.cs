c2c0a0403f495534e777df301977abf45e24d694
efremx <helloefrema@gmail.com>
2024-09-27 11:28:57 -0400

    Clock in unity

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Awake()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
